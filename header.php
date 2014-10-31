0adbf0d9c5d35813160b4986ea722ed13571e50d
MechaCoder <talktome1224@gmail.com>
2014-10-31 12:44:11 +0000

    fixed if statement [line 2].

--- header.php
+++ header.php
@@ -1,17 +1,18 @@
 <head>
-	<!-- theis is a work m8 header for bootstrap.
-	#php if(defined($titleVal)){}else{$titleVal = "Untitled";}?> -->
+	<?php if(defined($titleVal)){}else{$titleVal = "Untitled2";} ?>
 	<title><?php echo $titleVal; ?></title>
 	<!-- CSS import -->
 	<link rel="stylesheet" type="text/css" href="css\bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css\bootstrap-theme.css">
 	<link rel="stylesheet" type="text/css" href="css\bootstrap-theme.min.css">
-	<link rel="stylesheet" type="text/css" href="css\style.css">
+	<link rel="stylesheet" type="text/css" href="css\style.css"> <!-- this is where a devoper can can coustom css and call inderpently -->
 	<!-- JarvaScript import -->
 	<script type="text/javascript" src="js\bootstrap.js"></script>
 	<script type="text/javascript" src="js\bootstrap.min.js"></script>
 	<script type="text/javascript" src="js\npm.js"></script>
 	<!-- jquery input -->
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Orgnial Devoper Matthew Proudman -->
+	<!-- used by [yourname] -->
 </head>
