4f51d7c3ad5e18d7f563ab3beb0b3bea37035d96
MechaCoder <talktome1224@gmail.com>
2014-10-31 12:13:55 +0000

    the header.php is for the bootstrap web framework this ties in all the framework to html page by provideing header.php

---
+++ header.php
@@ -0,0 +1,17 @@
+<head>
+	<!-- theis is a work m8 header for bootstrap.
+	#php if(defined($titleVal)){}else{$titleVal = "Untitled";}?> -->
+	<title><?php echo $titleVal; ?></title>
+	<!-- CSS import -->
+	<link rel="stylesheet" type="text/css" href="css\bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css\bootstrap-theme.css">
+	<link rel="stylesheet" type="text/css" href="css\bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css\style.css">
+	<!-- JarvaScript import -->
+	<script type="text/javascript" src="js\bootstrap.js"></script>
+	<script type="text/javascript" src="js\bootstrap.min.js"></script>
+	<script type="text/javascript" src="js\npm.js"></script>
+	<!-- jquery input -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
