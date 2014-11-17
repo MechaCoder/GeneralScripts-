3472206a6bc2bdedc61be321205071a7aed62b60
MechaCoder <talktome1224@gmail.com>
2014-11-17 19:10:51 +0000

    headerCDN built to enable the CDN use of bootstrap

---
+++ headerCDN.php
@@ -0,0 +1,7 @@
+<head>
+	<title><?php echo titleVal; ?></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<link src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js">
+	<!-- this script is to use the CDN when useing bootstrap. -->
+</head>
