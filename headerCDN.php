35a11f1ae4d7959179fdc92ffa70c281450ee25f
MechaCoder <talktome1224@gmail.com>
2014-11-17 19:13:52 +0000

    adding a Jquery it should work

--- headerCDN.php
+++ headerCDN.php
@@ -3,5 +3,6 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 	<link src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<!-- this script is to use the CDN when useing bootstrap. -->
 </head>
