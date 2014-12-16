6c91febf9d8441dec9ac015e5d21dc14c3e54e3d
MechaCoder <talktome1224@gmail.com>
2014-12-16 23:14:34 +0000

    a few imporvements to the CDN this can call the signal class

--- headerCDN.php
+++ headerCDN.php
@@ -1,4 +1,3 @@
-<head>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 	<link src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js">
@@ -6,5 +5,8 @@
 	<!-- this script is to use the CDN when useing bootstrap. -->
 	<script type="text/javascript" src="http://www.parsecdn.com/js/parse-1.3.2.min.js"></script>
 	<script>
-		Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY");
+		Parse.initialize("APPLICATION_ID", "JAVASCRIPT_KEY"); // you get this from your quickstart guide, pls makesure that you are you the JarvaScript documenttion
+
+		var table = Parse.Object.extend("[your parse 'class' name here]");
+			var q = new Parse.Query(table); 
 	</script>
